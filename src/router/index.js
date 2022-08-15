30012bcd721263a9cfc09252722edfeee4adbb83
sanal1291 <54595945+sanal1291@users.noreply.github.com>
2022-08-16 02:38:29 +0530

    arbox studio route added

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import InitViewVue from '../views/InitView.vue'
+import ArtBoxVue from '../views/artbox.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: InitViewVue
+  },
+  {
+    path: '/3dview',
+    name: '3dview',
+    component: ArtBoxVue
   }
 ]
 
